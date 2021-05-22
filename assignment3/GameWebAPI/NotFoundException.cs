cb581acf5f2479b384a52534c338723271c6aae2
Dlegend123 <82322425+Dlegend123@users.noreply.github.com>
2021-05-22 18:16:01 -0500

    Add files via upload

---
+++ assignment3/GameWebAPI/NotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GameWebAPI
+{
+    [Serializable]
+    internal class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
