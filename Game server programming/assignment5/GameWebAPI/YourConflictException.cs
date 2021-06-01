af9bfc0111386cbe4cb41b35377ddad80170ecce
Dlegend123 <82322425+Dlegend123@users.noreply.github.com>
2021-06-01 14:11:33 -0500

    Add files via upload

---
+++ Game server programming/assignment5/GameWebAPI/YourConflictException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GameWebAPI
+{
+    [Serializable]
+    internal class YourConflictException : Exception
+    {
+        public YourConflictException()
+        {
+        }
+
+        public YourConflictException(string message) : base(message)
+        {
+        }
+
+        public YourConflictException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected YourConflictException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
