a28964125d6a3e0b42ec145d8ab6df647c6860d6
Dlegend123 <82322425+Dlegend123@users.noreply.github.com>
2021-06-01 14:09:52 -0500

    Add files via upload

---
+++ Game server programming/assignment5/GameWebAPI/NewItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameWebAPI
+{
+    public class NewItem
+    {
+        public DateTime CreationDate { get; set; }
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public int price { get; set; }
+		[Range(1, 99)]
+		public int ItemLevel { get; set; }
+		[EnumDataType(typeof(ItemType))]
+		public ItemType Type{ get; set; }
+		public Guid OwnerId { get; set; }
+    }
+}
